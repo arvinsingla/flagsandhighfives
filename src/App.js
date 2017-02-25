0bf73be4bd2e08064d67c428bff23f87f368a4bf
Arvin Singla <arvin.singla@acquia.com>
2017-02-25 12:49:44 -0500

    Updated design with new images and styles.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
 import hero from './fahf_website_hero.png';
+import arvin from './arvin.jpg';
+import ben from './ben.jpg';
 import './App.css';
 
 const episodeURL = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.soundcloud.com%2Fusers%2Fsoundcloud%3Ausers%3A290085134%2Fsounds.rss&api_key=0tjef0mdha7ryfkqk9hoovmgwz7oweknjaoksobz';
@@ -68,18 +69,48 @@ class App extends Component {
       <div className="app-wrapper">
         <header className="header">
           <div className="branding">
-            <img className="branding__logo" alt="Flags and High Fives logo" src={logo} />
+            <img className="branding__hero" alt="Flags and High Fives logo" src={hero} />
             <hgroup className="branding__text">
               <h1 className="branding__text__title">FLAGS AND HIGH FIVES</h1>
               <h2 className="branding__text__subtitle">A podcast retrospective</h2>
+              <p>Friends and former podcasters extraordinaire, Arvin and Ben, look back on their success, shortcomings, and the ultimate downfall of their hit 2007 video game podcast. </p>
+              <hr className="branding__text__separator" />
+              <h3 className="branding__text__newsletter__title">Subscribe</h3>
+              <p className="branding__text__newsletter_text">
+                Get notified of the latest episodes and news. We promise not to spam you.
+              </p>
+              <form
+                action="http://arvinsingla.us1.list-manage.com/subscribe/post?u=b89ca2fcacbd5d9a28518d5cd&amp;id=5443a024ee"
+                method="post"
+                id="mc-embedded-subscribe-form"
+                name="mc-embedded-subscribe-form"
+                className="branding__text__newsletter__form"
+                target="_blank"
+                noValidate
+              >
+                <input
+                  type="email"
+                  placeholder="Email address"
+                  value={this.state.emailAddress}
+                  onChange={(evt) => this.setState({emailAddress: evt.target.value})}
+                  name="EMAIL"
+                  className="required email"
+                  id="mce-EMAIL"
+                />
+                <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
+                  <input type="text" name="b_b89ca2fcacbd5d9a28518d5cd_5443a024ee" tabIndex="-1" value="" />
+                </div>
+                <input
+                  type="submit"
+                  value="Subscribe"
+                  name="subscribe"
+                  id="mc-embedded-subscribe"
+                  className="button"
+                  disabled={!validEmail}
+                />
+              </form>
             </hgroup>
           </div>
-          <nav className="navigation">
-            <ul>
-              <li><a href="https://www.instagram.com/flagsandhighfives/" target="_BLANK" className='instagram'>Instagram</a></li>
-              <li><a href="https://twitter.com/fahfpodcast" target="_BLANK" className="twitter">Twitter</a></li>
-            </ul>
-          </nav>
         </header>
 
         <div className={layoutWrapperClass}>
@@ -100,45 +131,24 @@ class App extends Component {
             })}
           </section>
           <aside className="sidebar">
-            <section className="newsletter">
-              <h3 className="newsletter__title">Newsletter</h3>
-              <p className="newsletter__description">Get notified of the latest episodes and news. We promise not to harrass you.</p>
-              <div id="mc_embed_signup" className="newsletter__form">
-                <form
-                  action="http://arvinsingla.us1.list-manage.com/subscribe/post?u=b89ca2fcacbd5d9a28518d5cd&amp;id=5443a024ee"
-                  method="post"
-                  id="mc-embedded-subscribe-form"
-                  name="mc-embedded-subscribe-form"
-                  className="validate"
-                  target="_blank"
-                  noValidate
-                >
-                  <div id="mc_embed_signup_scroll">
-                    <div className="mc-field-group">
-                      <input
-                        type="email"
-                        placeholder="Email address"
-                        value={this.state.emailAddress}
-                        onChange={(evt) => this.setState({emailAddress: evt.target.value})}
-                        name="EMAIL"
-                        className="required email"
-                        id="mce-EMAIL"
-                      />
-                    </div>
-                    <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-                      <input type="text" name="b_b89ca2fcacbd5d9a28518d5cd_5443a024ee" tabIndex="-1" value="" />
-                    </div>
-                      <input
-                        type="submit"
-                        value="Subscribe"
-                        name="subscribe"
-                        id="mc-embedded-subscribe"
-                        className="button"
-                        disabled={!validEmail}
-                      />
-                    </div>
-                </form>
-              </div>
+            <section className="social">
+              <ul>
+                <li><a href="https://www.instagram.com/flagsandhighfives/" target="_BLANK" title="Instagram" alt="Instagram" className='instagram'>Instagram</a></li>
+                <li><a href="https://twitter.com/fahfpodcast" target="_BLANK" className="twitter" title="Twitter" alt="Twitter">Twitter</a></li>
+                <li><a href="https://www.facebook.com/FAHFpodcast/" target="_BLANK" className="facebook" alt="facebook" title="facebook">Facebook</a></li>
+              </ul>
+            </section>
+            <section className="hosts">
+              <h3 className="hosts_title">Hosts</h3>
+              <section className="arvin">
+                <img src={arvin} alt="Arvin" />
+                <div className="description"><strong>Arvin</strong> is a software engineer, father, tinkerer and overall geeky guy. When he has time he writes <a href="https://medium.com/@arvinsingla" target="_BLANK">medium posts</a> about technology</div>
+              </section>
+              <section className="ben">
+                <img src={ben} alt="Ben" />
+                <div className="description"><strong>Ben</strong> tells stories. He's a writer and publisher with <a href="https://adventureworldspress.com/" target="_BLANK">Adventure Worlds Press</a>. His first books is out now. You can buy it and read more from him <a href="http://www.benvandongen.com" target="_BLANK">on his website</a>.</div>
+              </section>
+
            </section>
          </aside>
         </div>
